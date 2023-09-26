03cf64f584ed3daec0cc0a96e2ef24e2972fc6ff
John Abdou <68866305+jpabdou@users.noreply.github.com>
2023-09-26 13:58:53 -0700

    Add comments to AppRatePlot

--- src/app/components/AppRatePlot.tsx
+++ src/app/components/AppRatePlot.tsx
@@ -8,45 +8,40 @@ interface props {
 }
 
 export default function AppRatePlot(props : props) {
+  // passing plotData is array in the form of [{"_id": <date of week start>, "count": <application count for week>},...]
+  // weeks is array in the form of [<date of week start>,...] for the past 16 weeks
     const {weeks, plotData} = props;
     const [data, setData] = useState<{x: string[], y: number[]}>({x:[], y:[]});
-    const Plot = dynamic(()=>import("react-plotly.js"), {ssr:false,   loading: () => <p>Loading...</p>})
+    const Plot = dynamic(()=>import("react-plotly.js"), {ssr:false,   loading: () => <p>Loading...</p>});
+
+    useEffect(()=>{
+      if (plotData.length > 0){
+      let i: number = 0;
+      // loop to iterate over past 16 weeks date strings array (weeks) and plotData array to check if there's any missing weeks and add a 0-count if there are
+      // all points from plotData added to data state where data.x is an array of date strings and data.y is an array of application count numbers
+      for (let point of plotData!) {
+        if (point._id !== weeks![i]){
+          // if a week is not found, add missing week to data.x array and push 0 to data.y array by default
+          while (point._id !== weeks![i] && i<weeks!.length) {
+            data.x.push(weeks![i]);
+            data.y.push(0);
+            i++;
+          }};
+          data.x.push(point._id);
+          data.y.push(point.count); 
+        i++;
+      };
+      setData({...data})};
+      }, [plotData]);
 
 
-      interface IHashMap {
-          [key: string]: boolean
-       }
-
-
-
-      useEffect(()=>{
-        if (plotData.length > 0){
-        let map : IHashMap = {}
-        let i: number = 0
-        for (let point of plotData!) {
-          if (point._id !== weeks![i]){
-            // if a week is not found, add missing week to data.x array and push 0 to data.y array by default
-            while (point._id !== weeks![i] && i<weeks!.length) {
-              data.x.push(weeks![i]);
-              data.y.push(0);
-              i++
-            }}
-            data.x.push(point._id);
-            data.y.push(point.count); 
-          i++;
-        }
-        setData({...data})}
-       }, [plotData])
-
-
-
-       const [hasMounted, setHasMounted] = useState(false);
-       useEffect(() => {
-         setHasMounted(true);
-       }, []);
-       if (!hasMounted) {
-         return null;
-       }
+      const [hasMounted, setHasMounted] = useState(false);
+      useEffect(() => {
+        setHasMounted(true);
+      }, []);
+      if (!hasMounted) {
+        return null;
+      };
 
     return(
       <>
